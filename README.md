5593276cadf12c97ea35c30d685793406a741c76
Kevin Whitaker <kevin.whitaker@purediscovery.com>
2015-09-16 10:26:40 -0500

    Updating the readme and resetting the function

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # redux-make-immutable
 Redux middleware to coerce native javascript types into the equivalent Immutable.js types
+
+### Install
+`npm I -s redux-make-immutable`
+
+### Usage
+```
+import makeImmutable from 'redux-make-immutable'
+... applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware,
+      makeImmutable
+    );
+```
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import { Iterable, fromJS } from 'immutable';
 
-const makeImmutable = store => next => action => {
-  let result;
-  if (!Iterable.isIterable(action.payload)) {
-    let newAction = Object.assign({}, action,
-      {
-        payload: fromJS(action.payload)
-      }
-    );
-    result = next(newAction);
-  } else {
-    result = next(action);
-  }
+export default function makeImmutable() {
+  return next => action => {
+    let result;
+    if (!Iterable.isIterable(action.payload)) {
+      const newAction = Object.assign({}, action,
+        {
+          payload: fromJS(action.payload),
+        }
+      );
+      result = next(newAction);
+    } else {
+      result = next(action);
+    }
 
-  return result;
-};
-
-export default makeImmutable;
+    return result;
+  };
+}
