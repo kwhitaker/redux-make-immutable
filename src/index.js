89e745668b38e4d205a6ac8355f01d00edc779fd
Kevin Whitaker <kevin.whitaker@purediscovery.com>
2015-09-16 10:20:08 -0500

    Reverting to another call

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import { Iterable, fromJS } from 'immutable';
 
-export default function makeImmutable() {
-  return next => action => {
-    let result;
-    if (!Iterable.isIterable(action.payload)) {
-      const newAction = Object.assign({}, action,
-        {
-          payload: fromJS(action.payload),
-        }
-      );
-      result = next(newAction);
-    } else {
-      result = next(action);
-    }
+const makeImmutable = store => next => action => {
+  let result;
+  if (!Iterable.isIterable(action.payload)) {
+    let newAction = Object.assign({}, action,
+      {
+        payload: fromJS(action.payload)
+      }
+    );
+    result = next(newAction);
+  } else {
+    result = next(action);
+  }
 
-    return result;
-  };
-}
+  return result;
+};
+
+export default makeImmutable;
